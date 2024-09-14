d1b1381895d52b393f102801f757f941d9e28297
Balamuruganhit <balamuruganb011@gmail.com>
2024-09-14 13:16:58 +0530

    changes

--- scripts/load_demo_data.sh
+++ scripts/load_demo_data.sh
@@ -16,7 +16,7 @@ if [ -n "$RUNNING_OFBIZ_SERVICE_ID" ]; then
 fi
 
 echo Loading demo data...
-docker compose run --rm ofbiz loadDefault
+docker compose run --rm ofbiz loadAll
 echo Demo data loaded.
 
 echo Administrative user name: localadmin
