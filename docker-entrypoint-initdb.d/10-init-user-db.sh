c92791522d7c099e1007b11827d8d49882b9f3c2
Balamuruganhit <balamuruganb011@gmail.com>
2024-09-14 18:13:06 +0530

    change

--- docker-entrypoint-initdb.d/10-init-user-db.sh
+++ docker-entrypoint-initdb.d/10-init-user-db.sh
@@ -20,7 +20,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE ofbiz_tenant001 TO tenant1_user;
     GRANT ALL PRIVILEGES ON DATABASE ofbizolap_tenant001 TO tenant1_user;
 
-    CREATE USER tenant1_user WITH PASSWORD 'tenant1_pass';
+    CREATE USER tenant2_user WITH PASSWORD 'tenant1_pass';
     CREATE DATABASE ofbiz_tenant002;
     CREATE DATABASE ofbizolap_tenant002;
     GRANT ALL PRIVILEGES ON DATABASE ofbiz_tenant002 TO tenant1_user;
