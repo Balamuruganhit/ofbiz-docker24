32ba0f06ff479254ca2d605494fd30df80ce5a95
Balamuruganhit <balamuruganb011@gmail.com>
2024-09-14 16:44:55 +0530

    change

--- scripts/load_demo_data.sh
+++ scripts/load_demo_data.sh
@@ -18,7 +18,7 @@ fi
 echo Loading demo data...
 docker compose run --rm ofbiz createTenant -PtenantId=tenant001   -PtenantName="My Tenant 001" -PdomainName=tenant001.example.com -PtenantReaders=seed,seed-initial,ext -PdbPlatform=P -PdbIp=127.0.0.1 -PdbUser=ofb_tenant001 -PdbPassword=ofbiz@tenant
 echo Demo data loaded.
- docker compose run --rm ofbiz loadTenantData -PtenantId=tenant001 -PtenantReaders=seed,seed-initial,demo --stacktrace --info
+ 
 
 echo Administrative user name: localadmin
 echo Administrative user password: ofbiz
